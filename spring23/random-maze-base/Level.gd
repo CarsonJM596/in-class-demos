e974396e7ef05090cb1529409ed487f8fb4e7e1b
Nathan Eloe <powerofazure@gmail.com>
2023-04-07 10:47:04 -0500

    Add random coin locations

--- spring23/random-maze-base/Level.gd
+++ spring23/random-maze-base/Level.gd
@@ -7,13 +7,22 @@ var border = Rect2(1, 1, 30, 17)
 var level_map
 
 export (PackedScene) var collectable
-const NUM_COINS = 1
+const NUM_COINS = 6
 
+# Bitmasking
+const BITS = [1,  2,   4,
+			  8,  16,  32, 
+			  64, 128, 256]
+
+var LOC_OFFSETS = {}
 
 func _ready():
 	randomize()
 	generate_level()
 	level_map.shuffle()
+	for i in 3:
+		for j in 3:
+			LOC_OFFSETS[BITS[3*i+j]] = Vector2.ONE + Vector2(j,i)
 	add_collectables()
 	#$Player.position = Vector2(15*32 + 16, 17*32+16)
 	$Player.position = level_map[NUM_COINS] * 32 + Vector2(16,16)
@@ -34,7 +43,19 @@ func generate_level():
 func add_collectables():
 	for i in NUM_COINS:
 		var coin = collectable.instance()
-		coin.position = level_map[i]*32 + Vector2(16,16)
+		coin.position = level_map[i]*32 + gen_offset(level_map[i])
 		#print($Dungeon.get_cell_autotile_coord(level_map[i].x, level_map[i].y))
 		#print($Dungeon.get_tileset().autotile_get_bitmask(0, $Dungeon.get_cell_autotile_coord(level_map[i].x, level_map[i].y)))
 		add_child(coin)
+
+func gen_offset(loc):
+	var at_coord = $Dungeon.get_cell_autotile_coord(loc.x, loc.y)
+	var bitmask = $Dungeon.get_tileset().autotile_get_bitmask(0, at_coord)
+	print(at_coord, bitmask)
+	var masks = BITS.duplicate()
+	masks.shuffle()
+	var mask = masks.pop_back()
+	while not mask & bitmask:
+		mask = masks.pop_back()
+	
+	return LOC_OFFSETS[mask] * 8
