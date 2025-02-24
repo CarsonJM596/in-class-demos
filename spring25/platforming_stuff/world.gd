778d69208f087e6a9b947c4b7f0288ffcf7e9d06
Nathan Eloe <powerofazure@gmail.com>
2025-02-24 12:57:06 -0600

    Add terrain and initial background

---
+++ spring25/platforming_stuff/world.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	#print($Player/Camera2D.global_position)
+	#$Player/Camera2D.limit_left += 1
+
+
+func _on_kill_plane_body_entered(body: Node2D) -> void:
+	$Player.position = $PlayerStartPosition.position
